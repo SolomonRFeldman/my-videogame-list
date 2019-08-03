c0ac560ede18c0922c52586e22b936cd16ee9407
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-08-03 09:56:47 -0700

    adds validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :games, through: :user_games
   has_many :posts
   has_many :ratings
+  
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
 
-  has_secure_password
 end
