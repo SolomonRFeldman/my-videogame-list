ba62ccfcb6544efcff7cc86a4ee494eeda7b9954
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-08-02 18:11:45 -0700

    comments update rating idea

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ActiveRecord::Base
+  #update rating will multiply current number of ratings by rating, add in the new one, and divide by new number of ratings
   has_many :users, through: :user_games
   has_many :posts
   has_many :ratings
