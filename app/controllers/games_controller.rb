26e0cc4e5bb0f8b3563dfad662f26c53e53adf55
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-08-04 15:08:47 -0700

    adds adding new games and games to users

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,24 @@
 class GamesController < ApplicationController
+
+  get '/games/new' do
+    if session[:user_id]
+      erb :'games/new'
+    else
+      redirect '/'
+    end
+  end
+
+  post '/games' do    
+    if user = User.find(session[:user_id])
+      if !user.games.find_by(params[:game])
+        game = Game.find_by(params[:game]) || Game.create(params[:game])
+        user.games << game
+        redirect "/users/#{slug(user.username)}"
+      else
+        redirect '/games/new'
+      end
+    end
+  end
+
+
 end
