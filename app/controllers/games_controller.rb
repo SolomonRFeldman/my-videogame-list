ff00b71f9c3cfbfe26efcff9c499c3f37cd01f11
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-08-23 12:48:52 -0400

    adds adding posts to games from its show and links

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,6 +10,29 @@ class GamesController < ApplicationController
     @rating = Feed.game_rating.where("games.name = '#{@game.name}' AND ratings.current_rating = 'true'").average("ratings.rating")
     erb :'/games/show'
   end
+  
+  get '/games/:slug/new' do
+    if @current_user && @game = Game.find_by(name: unslug(params[:slug]))
+      erb :'games/new'
+    else
+      redirect '/'
+    end
+  end
+
+  post "/games/:slug/" do
+    if @current_user
+      unless game = @current_user.games.find_by(name: unslug(params[:slug]))
+        game = Game.find_by(name: unslug(params[:slug]))
+        unplayed = true
+      end
+      if game
+        rating = Rating.create(user_id: @current_user.id, game_id: game.id, rating: params[:rating]) if params[:rating]
+        post = Post.create(user_id: @current_user.id, game_id: game.id, content: params[:post][:content], rating: rating) if params[:post][:content]
+        UserGame.create(user_id: @current_user.id, game_id: game.id, post: post, rating: rating) if unplayed
+        redirect "/games/#{slug(game.name)}"
+      end
+    end
+  end
 
 
 end
