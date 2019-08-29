24e2297e32f94f1e4d1c7dfe69013101be3eb097
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-08-29 00:17:33 -0400

    fixes exception

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,4 @@
 class GamesController < ApplicationController
-
-  get '/games' do
-    erb :'/games/index'
-  end
   
   get '/games/:slug' do
     @game = Game.find_by(name: unslug(params[:slug]))
