8df47603aa18b1f2d213ed35c413dd468d51cc8d
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-08-30 16:47:45 -0400

    adds attributes

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,9 @@
 class Activity < ActiveRecord::Base
-
+  belongs_to :user
+  belongs_to :game
+  
+  validates :user_id, presence: true
+  validates :game_id, presence: true
+  validates :user_id, uniqueness: { scope: [:game_id, :current_rating] }, if: :current_rating
+  validates :user_id, uniqueness: { scope: [:game_id, :played] }, if: :played
 end
