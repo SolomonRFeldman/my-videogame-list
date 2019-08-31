4244ba57e4920871f407bcdf5407e5a4cbdd9130
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-08-31 14:25:29 -0400

    fixes load order

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require 'require_all'
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'pry'
+require_relative '../app/controllers/application_controller'
 require_all 'app'
 
 set :database_file, './database.yml'
