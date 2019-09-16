927d24ae62acf1754d858901ecfc3c1bedc5cd7d
Solomon R Feldman <SolomonRFeldman@gmail.com>
2019-09-15 20:02:11 -0400

    refactors missing game name error

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -10,19 +10,21 @@ class ActivitiesController < ApplicationController
 
   post "/activities" do
     if @current_user
-      unless game = @current_user.games.find_by(params[:game])
-        game = Game.find_by(params[:game]) || Game.create(params[:game])
+      unless @game = @current_user.games.find_by(params[:game])
+        @game = Game.find_by(params[:game]) || Game.create(params[:game])
         params[:activity][:played] = true
       end
-      if game.valid?
+      if @game.valid?
         params[:activity][:user_id] = @current_user.id
-        params[:activity][:game_id] = game.id
+        params[:activity][:game_id] = @game.id
         Activity.create(params[:activity])
         redirect "/users/#{slug(@current_user.username)}"
       end
-      redirect "/users/#{slug(@current_user.username)}/new?emptygame=true"
+      erb :'/activities/new'
+    else
+      redirect '/'
     end
-    redirect '/'
+
   end
 
   get '/activities/:id/edit' do
